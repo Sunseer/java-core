a2bc8f89497bf93f3da7d7c57c0af5278c85c111
Sunseer <playvsme@gmail.com>
2016-11-10 20:55:32 +0400

    added Factorial and FactorialTest classes

---
+++ chapter_001/04/src/test/java/ru/agrin/FactorialTest.java
@@ -0,0 +1,16 @@
+package ru.agrin;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class FactorialTest
+{
+	@Test
+	public void testFactorial() {
+        	Factorial fact = new Factorial();
+        	long result = fact.factorial(5L);
+		assertThat(result, is (120L));
+	}
+
+}
