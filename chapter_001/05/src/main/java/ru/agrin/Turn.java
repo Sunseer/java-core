d84056c421b3114b504db8c7af0f3974c460ad42
Sunseer <playvsme@gmail.com>
2016-11-18 16:17:08 +0300

    added Turn and TurnTest classes

--- chapter_001/05/src/main/java/ru/agrin/Turn.java
+++ chapter_001/05/src/main/java/ru/agrin/Turn.java
@@ -4,4 +4,12 @@ package ru.agrin;
  * Created by grin on 18.11.2016.
  */
 public class Turn {
+    public int[] back(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+        return array;
+    }
 }
--- chapter_001/05/src/test/java/ru/agrin/TurnTest.java
+++ chapter_001/05/src/test/java/ru/agrin/TurnTest.java
@@ -1,7 +1,22 @@
 package ru.agrin;
 
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
 /**
  * Created by grin on 18.11.2016.
  */
 public class TurnTest {
+    @Test
+    public void testBack() {
+        int[] array = new int[] {1, 2, 3, 4, 5};
+        int[] arrayResult = array.clone();
+        Turn turn = new Turn();
+        arrayResult = turn.back(arrayResult);
+
+        for (int i = 0; i < array.length; i++) {
+            assertThat(arrayResult[i], is (array[array.length - 1 - i]));
+        }
+    }
 }
