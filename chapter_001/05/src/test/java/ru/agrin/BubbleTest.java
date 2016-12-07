bc1ef0bcf0f182764a28d8320e7a1b1c4e11d72e
Sunseer <playvsme@gmail.com>
2016-12-07 16:23:06 +0300

    added class to merge sorted arrays

--- chapter_001/05/src/test/java/ru/agrin/BubbleTest.java
+++ chapter_001/05/src/test/java/ru/agrin/BubbleTest.java
@@ -10,7 +10,7 @@ public class BubbleTest {
     @Test
     public void testBubbleSort() {
         int[] array = new int[] {1, 2, 4, 5, 8};
-	Bubble bubble = new Bubble();
+	    Bubble bubble = new Bubble();
         int[] result = bubble.bubbleSort(bubble.values);
         assertThat(result, is(array));
     }
